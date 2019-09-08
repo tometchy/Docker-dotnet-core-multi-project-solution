26902706d8d52021ad8d82beb9c44625d8603e41
Tometchy <tometchy@gmail.com>
2019-09-08 02:47:25 +0200

    Add example asp net core app

---
+++ Aspnetcoreapp/Pages/Privacy.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace aspnetcoreapp.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
