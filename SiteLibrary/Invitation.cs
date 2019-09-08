db0d092c0e69227e7cd6664628ba18093d6cd130
Tometchy <tometchy@gmail.com>
2019-09-08 03:04:48 +0200

    Create invitation dependency

---
+++ SiteLibrary/Invitation.cs
@@ -0,0 +1,14 @@
+﻿namespace SiteLibrary
+{
+    public class Invitation
+    {
+        public string Text { get; }
+        public string Style { get; }
+
+        public Invitation(string text, string style)
+        {
+            Text = text;
+            Style = style;
+        }
+    }
+}
